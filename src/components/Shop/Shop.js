9c5f6e502c4c40d8fe414ac02abd64482083bd29
mukitur <mukit.cpl@gmail.com>
2021-09-25 09:06:45 +0600

    search implemented

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,10 +8,14 @@ import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+    const [displayProducts, setDisplayProducts] = useState([]);
     useEffect(() => {
         fetch('./products.json')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data)
+                setDisplayProducts(data)
+            })
     }, [])
 
     //we stored data in local storage but , when refreshed it gone from cart, to solve the issure:
@@ -37,27 +41,41 @@ const Shop = () => {
         addToDb(product.key);
     }
 
+    const handleSearch = event => {
+        const searchText = event.target.value;
+
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+
+        setDisplayProducts(matchedProducts);
+    }
+
     return (
-        <div className="shop-container">
-            <div className="product-container">
-                {
-                    products.map(product => <Product
-                        key={product.key}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
+        <>
+            <div className="search-container">
+                <input onChange={handleSearch} type="Text" placeholder="Search Product" />
             </div>
 
-            <div className="cart-container">
+            <div className="shop-container">
+                <div className="product-container">
+                    {
+                        displayProducts.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
 
-                <Cart
-                    cart={cart}
-                ></Cart>
+                <div className="cart-container">
+
+                    <Cart
+                        cart={cart}
+                    ></Cart>
+
+                </div>
 
             </div>
-
-        </div>
+        </>
     );
 };
 
